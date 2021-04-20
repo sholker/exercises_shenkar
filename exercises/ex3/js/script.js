0e3037f1363ede84d7caee8cc487ee1b5270c923
oris <oris1002@gmail.com>
2021-04-20 20:14:33 +0300

    remove dup

--- exercises/ex3/js/script.js
+++ exercises/ex3/js/script.js
@@ -50,20 +50,12 @@ function random_color(){
          var color = Math.floor(Math.random() * colors.length);
          return colors[color]
 }
-function color_boxes(){
-    var myColors = [
-        '#7F8C8D', '#95A5A6', '#BDC3C7', '#003946', '#BDC3C7',
-        '#ECF0F1', '#BDC3C7', '#ECF0F1', '#C0392B', '#E74C3C',
-        '#D35400', '#E67E22', '#F39C12', '#F1C40F', '#22313f',
-        '#2C3E50', '#34495E', '#8E44AD', '#9B59B6', '#2980B9',
-        '#3498DB', '#27AE60', '#2ECC71', '#16A085'
-    ];
 
-        var colors = myColors.slice(0);
-   
+// all div get color
+function color_boxes(){
+
         $('div.grid').each(function() {
-             var color = Math.floor(Math.random() * colors.length);
-             $(this).css('background-color', colors[color]);
+             $(this).css('background-color', random_color());
 
         });
 
