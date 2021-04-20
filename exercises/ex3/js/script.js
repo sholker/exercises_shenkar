125a52501e57d1fc2acd2c03c6352b7a771bd80a
oris <oris1002@gmail.com>
2021-04-20 22:49:04 +0300

    updated

--- exercises/ex3/js/script.js
+++ exercises/ex3/js/script.js
@@ -1,18 +1,19 @@
-function createGrid(x) {
+var count = 0 ;
 
-    count = 0 ;
+function createGrid(x) {
+    console.log(count);
     while (count < x){
         for (var columns = 0; columns < 3; columns++) {
             count++;
-            console.log(x)
+
             if (count > x)
                 break;
 
             if (count == 1 ){ //the first
-               $("#container").append("<div style='pointer-events: none'; class='grid'><div id='plus'></div></div>");
+               $("#container").append("<div class='grid' style='pointer-events: none';><div id='plus' ></div></div>");
                 continue;
             }
-            if ((columns+1) % 3 == 0 ){
+            if ((count) % 3 == 0 ){
 
                $("#container").append("<div class='grid'><div id='star'></div></div>");
 
@@ -25,7 +26,7 @@ function createGrid(x) {
     };
     if ( screen.width < 960){
             $(".grid").width(960/2);
-    $(".grid").height(960/2);
+            $(".grid").height(960/2);
     }
     else {
         $(".grid").width(960 / 3);
@@ -34,9 +35,8 @@ function createGrid(x) {
     color_boxes()
 };
 
-function clearGrid(){
-    $(".grid").remove();
-};
+
+
 
 function random_color(){
         var myColors = [
@@ -49,27 +49,65 @@ function random_color(){
          var colors = myColors.slice(0);
          var color = Math.floor(Math.random() * colors.length);
          return colors[color]
-}
+};
 
 // all div get color
 function color_boxes(){
 
         $('div.grid').each(function() {
-             $(this).css('background-color', random_color());
+
+            if ( $(this).css('backgroundColor').toString() == "rgba(0, 0, 0, 0)" ) {
+             $(this).css('background-color', random_color());}
 
         });
 
 
+    };
+
+function add_box(){
+    console.log(count);
+
+    if ((count%3) == 0 ){
+
+       $("#container").append("<div class='grid'><div id='star'></div></div>");
+
+    }
+    else {
+       $("#container").append("<div class='grid'></div>");
+
     }
 
-
+    count++;
+    if ( screen.width < 960){
+            $(".grid").width(960/2);
+            $(".grid").height(960/2);
+    }
+    else {
+        $(".grid").width(960 / 3);
+        $(".grid").height(960 / 3);
+    }
+    color_boxes();
+};
 
 $(document).ready(function() {
     str = "shai"
     createGrid(str.length*2);
 
     $(".grid").click(function() {
+         var plus_found = false;
 
+         $(this).find("div[id^=plus]").each(function() {
+              add_box();
+              plus_found = true;
+
+
+             return;
+      });
+
+
+
+    if( !plus_found )
+    {
         if ($(this).attr('id') != 'theImg') {
 
             $(this).css("background-color", "white");
@@ -79,10 +117,43 @@ $(document).ready(function() {
             $(this).removeAttr('id');
             $(this).css("background-color", random_color());
         }
+    }
         });
 
-    $("#plus").click(function (){
-        alert("added");
-    });
 
-});
+
+})
+
+// $(document).click(function() {
+//
+//
+//     $(".grid").click(function() {
+//          var plus_found = false;
+//
+//          $(this).find("div[id^=plus]").each(function() {
+//               add_box();
+//               plus_found = true;
+//
+//
+//              return;
+//       });
+//
+//
+//
+//     if( !plus_found )
+//     {
+//         if ($(this).attr('id') != 'theImg') {
+//
+//             $(this).css("background-color", "white");
+//             $(this).attr('id', 'theImg');
+//         }
+//         else {
+//             $(this).removeAttr('id');
+//             $(this).css("background-color", random_color());
+//         }
+//     }
+//         });
+//
+//
+//
+// });
